8ee45e6e2ec622ecd6423711b4c3cbaa8f7cd579
Atadan İÇEN <atadanicen@gmail.com>
2026-01-29 23:36:24 +0300

    feat: add error page for displaying status and message

---
+++ src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import * as Empty from '$lib/components/ui/empty/index.js';
+</script>
+
+<div class="flex grow flex-col p-4">
+	<Empty.Root>
+		<Empty.Header>
+			<Empty.Title>{page.status + ' ' + page.error?.message}</Empty.Title>
+			<Empty.Description>The page you're looking for doesn't exist.</Empty.Description>
+		</Empty.Header>
+	</Empty.Root>
+</div>
